1b5327b4b4e06911a57bcd45b1eae8f622a4068c
jake.torrance <jake.torrance@digital.hmrc.gov.uk>
2017-01-17 14:31:41 +0000

    .. and ~ optimizations

--- src/dir_search/dir_search.py
+++ src/dir_search/dir_search.py
@@ -7,20 +7,25 @@ class DirSearch():
         split = dir_string.split("/")
         self.search_string = split.pop()
         self.workdir = self.get_workdir(split)
-        self.regex = re.compile(re.escape(self.search_string))
+        self.regex = re.compile(self.search_string)
+
+    def problematic_search_string(self):
+        curr_path = os.getcwd() + "/" + self.search_string
+        resolved_path = os.path.realpath(self.search_string)
+        return (curr_path != resolved_path)
 
     def get_workdir(self, split):
         workdir = os.getcwd()
         if(split):
             full_path = "/".join(split)
             workdir = full_path
-            workdir = os.path.expanduser(full_path)
-            workdir = os.path.realpath(full_path)
+            workdir = os.path.expanduser(workdir)
+            workdir = os.path.realpath(workdir)
         return workdir
 
     def return_first_match(self):
         matched_dir = ""
-        if(self.search_string):
+        if(not self.problematic_search_string() and self.search_string):
             for root, dirs, files in os.walk(self.workdir):
                 found = False
                 if(root[-1] != "/"):
@@ -33,9 +38,10 @@ class DirSearch():
                 if(found):
                     break
         else:
-            matched_dir = self.workdir
-        if(not matched_dir):
-            matched_dir = self.search_string
-            matched_dir = os.path.expanduser(matched_dir)
-            matched_dir = os.path.realpath(matched_dir)
+            if(self.problematic_search_string()):
+                matched_dir = self.workdir + "/" + self.search_string()
+                matched_dir = os.path.expanduser(matched_dir)
+                matched_dir = os.path.realpath(matched_dir)
+            else:
+                matched_dir = self.workdir
         return matched_dir
