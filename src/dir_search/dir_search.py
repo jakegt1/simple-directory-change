c261b7e592fd592e0b0f98017806476fccb37549
jake.torrance <jake.torrance@digital.hmrc.gov.uk>
2017-01-17 14:04:19 +0000

    Cleaned up a little bit of code, can probably replace actual cd now.

--- src/dir_search/dir_search.py
+++ src/dir_search/dir_search.py
@@ -4,33 +4,38 @@ import re
 class DirSearch():
 
     def __init__(self, dir_string):
-        self.workdir = os.getcwd()
-        self.search_string = dir_string
-        if(dir_string[0] == "/" or dir_string[0] == "~"):
-            self.workdir = ""
         split = dir_string.split("/")
-        if(split[-1] == ''):
-            split.pop()
-        if(len(split) > 1):
-            self.search_string = split.pop();
-            self.workdir = self.workdir+"/"+'/'.join(split)
-            os.chdir(self.workdir)
-        else:
-            self.search_string = split[0]
-        self.regex = re.compile(self.search_string)
+        self.search_string = split.pop()
+        self.workdir = self.get_workdir(split)
+        self.regex = re.compile(re.escape(self.search_string))
 
+    def get_workdir(self, split):
+        workdir = os.getcwd()
+        if(split):
+            full_path = "/".join(split)
+            workdir = full_path
+            workdir = os.path.expanduser(full_path)
+            workdir = os.path.realpath(full_path)
+        return workdir
 
-    def return_matching_dirs(self):
-        matching_dirs = []
-        for root, dirs, files in os.walk(self.workdir):
-            found = False
-            if(root[-1] != "/"):
-                root += "/"
-            for dir in dirs:
-                if(self.regex.match(dir)):
-                    matching_dirs.append(root+dir)
-                    found = True
+    def return_first_match(self):
+        matched_dir = ""
+        if(self.search_string):
+            for root, dirs, files in os.walk(self.workdir):
+                found = False
+                if(root[-1] != "/"):
+                    root += "/"
+                for dir in dirs:
+                    if(self.regex.match(dir)):
+                        matched_dir = root+dir
+                        found = True
+                        break
+                if(found):
                     break
-            if(found):
-                break
-        return matching_dirs
+        else:
+            matched_dir = self.workdir
+        if(not matched_dir):
+            matched_dir = self.search_string
+            matched_dir = os.path.expanduser(matched_dir)
+            matched_dir = os.path.realpath(matched_dir)
+        return matched_dir
